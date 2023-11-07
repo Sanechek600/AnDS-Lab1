6348acad1f1ac80ff91ee782f597c86b4edadbc8
Sanechek600 <dassander@mail.ru>
2023-11-07 11:49:53 +0400

    Added task-specific function, works with all needed types

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <cstdlib>
 #include <ctime>
 
-using namespace std;
-
 template <typename T>
 class UniqueSet {
 private:
@@ -58,6 +56,10 @@ public:
 		_elements = nullptr;
 		_size = 0;
 		_capacity = 0;
+	}
+
+	size_t get_size() {
+		return _size;
 	}
 
 	// Оператор []
@@ -169,9 +171,16 @@ public:
 	}
 };
 
+std::ostream& operator<<(std::ostream& os, std::pair<int, double> p) {
+	os << "(" << p.first << ", " << p.second << ")";
+	return os;
+}
+
+//Returns 1 if all elements of sbj are in obj
 template <typename T>
-bool allElementsIn(const UniqueSet<T>& sbj, const UniqueSet<T>& obj) {
-	for (int i = 0; i < sbj._size; ++i) {
+//Here sbj in not const becuase compiler cannot guarantee it is unchanged when using get_size
+bool allElementsIn(UniqueSet<T>& sbj, const UniqueSet<T>& obj) {
+	for (int i = 0; i < sbj.get_size(); ++i) {
 		if (!obj.contains(sbj[i])) {
 			return false;
 		}
@@ -182,29 +191,63 @@ bool allElementsIn(const UniqueSet<T>& sbj, const UniqueSet<T>& obj) {
 int main() {
 	int intValues1[] = { 1, 2, 3, 4, 5 };
 	int intValues2[] = { 1, 2, 3, 4, 5, 6, 7 };
-	float floatValues1[] = { 1.5, 1.7, 2.5, 0.9, 5.7, 9.3 };
-	float floatValues2[] = { 1.5, 1.7, 2.5, 0.9 };
+	double floatValues1[] = { 1.5, 1.7, 2.5, 0.9, 5.7, 9.3 };
+	double floatValues2[] = { 1.5, 1.7, 2.5, 0.9 };
+	std::string strValues1[] = { "str1", "str2", "str3" };
+	std::string strValues2[] = { "str1", "str2", "str4", "str5" };
+	std::pair<int, double> pairValues1[] = { std::make_pair(5, 5.5), std::make_pair(2, 2.25) };
+	std::pair<int, double> pairValues2[] = { std::make_pair(2, 2.25), std::make_pair(3, 3.35), std::make_pair(5, 5.5) };
 
 	UniqueSet<int> intSet1(intValues1, sizeof(intValues1) / sizeof(intValues1[0]));
 	UniqueSet<int> intSet2(intValues2, sizeof(intValues2) / sizeof(intValues2[0]));
-	UniqueSet<float> floatSet1()
+	UniqueSet<double> floatSet1(floatValues1, sizeof(floatValues1) / sizeof(floatValues1[0]));
+	UniqueSet<double> floatSet2(floatValues2, sizeof(floatValues2) / sizeof(floatValues2[0]));
+	UniqueSet<std::string> strSet1(strValues1, sizeof(strValues1) / sizeof(strValues1[0]));
+	UniqueSet<std::string> strSet2(strValues2, sizeof(strValues2) / sizeof(strValues2[0]));
+	UniqueSet<std::pair<int, double>> pairSet1(pairValues1, sizeof(pairValues1) / sizeof(pairValues1[0]));
+	UniqueSet<std::pair<int, double>> pairSet2(pairValues2, sizeof(pairValues2) / sizeof(pairValues2[0]));
 
 	std::cout << "intSet1: ";
 	intSet1.display();
 	std::cout << "intSet2: ";
 	intSet2.display();
+	std::cout << std::endl;
+	std::cout << "floatSet1: ";
+	floatSet1.display();
+	std::cout << "floatSet2: ";
+	floatSet2.display();
+	std::cout << std::endl;
+	std::cout << "strSet1: ";
+	strSet1.display();
+	std::cout << "strSet2: ";
+	strSet2.display();
+	std::cout << std::endl;
+	std::cout << "strSet1: ";
+	strSet1.display();
+	std::cout << "strSet2: ";
+	strSet2.display();
+	std::cout << std::endl;
+	std::cout << "pairSet1: ";
+	pairSet1.display();
+	std::cout << "pairSet2: ";
+	pairSet2.display();
+	std::cout << std::endl;
 
 	UniqueSet<int> unionSet = intSet1 + intSet2;
 	std::cout << "Union: ";
 	unionSet.display();
 
-	UniqueSet<int> differenceSet = intSet1 - intSet2;
+	UniqueSet<int> differenceSet = intSet2 - intSet1;
 	std::cout << "Difference: ";
 	differenceSet.display();
 
 	UniqueSet<int> intersectionSet = intSet1.intersection(intSet2);
 	std::cout << "Intersection: ";
 	intersectionSet.display();
+
+	std::cout << "All elements of intSet1 are in intSet2: " << allElementsIn(intSet1, intSet2) << std::endl;
+	std::cout << "All elements of intSet2 are in intSet1: " << allElementsIn(intSet2, intSet1) << std::endl;
+	std::cout << "All elements of pairSet1 are in pairSet2: " << allElementsIn(pairSet1, pairSet2) << std::endl;
 
 	return 0;
 }
